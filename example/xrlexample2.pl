862f1e218356b5a62cde942afc40fdc273cec931
Tom Schoonjans <Tom.Schoonjans@UGent.be>
2012-08-17 15:28:50 +0200

    Perl bindings updated

--- example/xrlexample2.pl
+++ example/xrlexample2.pl
@@ -14,6 +14,8 @@
 
 #use strict;
 use xraylib;
+use Math::Trig;
+use Math::Complex;
 
 
 
@@ -31,6 +33,19 @@ printf("Pb Lalpha XRF production cs at 20.0 keV (jump approx): %f\n",xraylib::CS
 printf("Pb Lalpha XRF production cs at 20.0 keV (Kissel): %f\n",xraylib::CS_FluorLine_Kissel(82,$xraylib::LA_LINE,20.0));
 printf("Bi M1N2 radiative rate: %f\n",xraylib::RadRate(83,$xraylib::M1N2_LINE));
 printf("U M3O3 Fluorescence Line Energy: %f\n",xraylib::LineEnergy(92,$xraylib::M3O3_LINE));
+
+my $cdtest = xraylib::CompoundParser("Ca(HCO3)2");
+printf("Ca(HCO3)2 contains %i atoms and %i elements\n",$cdtest->{nAtomsAll},$cdtest->{nElements});
+for ($i = 0 ; $i < $cdtest->{nElements} ; $i++) {
+  printf("Element %i: %lf %%\n",$cdtest->{Elements}->[$i],$cdtest->{massFractions}->[$i]*100.0);
+}
+
+$cdtest = xraylib::CompoundParser("SiO2");
+printf("SiO2 contains %i atoms and %i elements\n",$cdtest->{nAtomsAll},$cdtest->{nElements});
+for ($i = 0 ; $i < $cdtest->{nElements} ; $i++) {
+  printf("Element %i: %lf %%\n",$cdtest->{Elements}->[$i],$cdtest->{massFractions}->[$i]*100.0);
+}
+
 printf("Ca(HCO3)2 Rayleigh cs at 10.0 keV: %f\n",xraylib::CS_Rayl_CP("Ca(HCO3)2",10.0) );
 printf("CS2 Refractive Index at 10.0 keV : %g - %g i\n",xraylib::Refractive_Index_Re("CS2",10.0,1.261),xraylib::Refractive_Index_Im("CS2",10.0,1.261));
 printf("C16H14O3 Refractive Index at 1 keV : %g - %g i\n",xraylib::Refractive_Index_Re("C16H14O3",1.0,1.2),xraylib::Refractive_Index_Im("C16H14O3",1.0,1.2));
@@ -46,10 +61,125 @@ printf("Au Mg XRF production cs at 10.0 keV (Kissel): %f\n", xraylib::CS_FluorLi
 
 printf("Bi L2-M5M5 Auger non-radiative rate: %g\n",xraylib::AugerRate(86,$xraylib::L2_M5M5_AUGER));
 
+
+$symbol = xraylib::AtomicNumberToSymbol(26);
+printf("Symbol of element 26 is: %s\n",$symbol);
+
+printf("Number of element Fe is: %i\n",xraylib::SymbolToAtomicNumber("Fe"));
+
 printf("Pb Malpha XRF production cs at 20.0 keV with cascade effect: %f\n",xraylib::CS_FluorLine_Kissel(82,$xraylib::MA1_LINE,20.0));
 printf("Pb Malpha XRF production cs at 20.0 keV with radiative cascade effect: %f\n",xraylib::CS_FluorLine_Kissel_Radiative_Cascade(82,$xraylib::MA1_LINE,20.0));
 printf("Pb Malpha XRF production cs at 20.0 keV with non-radiative cascade effect: %f\n",xraylib::CS_FluorLine_Kissel_Nonradiative_Cascade(82,$xraylib::MA1_LINE,20.0));
 printf("Pb Malpha XRF production cs at 20.0 keV without cascade effect: %f\n",xraylib::CS_FluorLine_Kissel_no_Cascade(82,$xraylib::MA1_LINE,20.0));
-  
+ 
+
+# Si Crystal structure 
+
+$cryst = xraylib::Crystal_GetCrystal("Si");
+if ($cryst == undef) {
+	printf("should not get here\n");
+  	exit 1;
+}
+
+printf ("Si unit cell dimensions are %f %f %f\n", $cryst->{a}, $cryst->{b}, $cryst->{c});
+printf ("Si unit cell angles are %f %f %f\n", $cryst->{alpha}, $cryst->{beta}, $cryst->{gamma});
+printf ("Si unit cell volume is %f\n", $cryst->{volume});
+printf ("Si atoms at:\n");
+printf ("   Z  fraction    X        Y        Z\n");
+for ($i = 0; $i < $cryst->{n_atom} ; $i++) {
+    $atom = ${cryst}->{atom}->[$i];
+    printf ("  %3i %f %f %f %f\n", $atom->{Zatom}, $atom->{fraction}, $atom->{x}, $atom->{y}, $atom->{z});
+}
+
+# Si diffraction parameters
+printf ("\nSi111 at 8 KeV. Incidence at the Bragg angle:\n");
+
+$energy = 8;
+$debye_temp_factor = 1.0;
+$rel_angle = 1.0;
+
+$bragg = xraylib::Bragg_angle($cryst, $energy, 1, 1, 1);
+printf ("  Bragg angle: Rad: %f Deg: %f\n", $bragg, $bragg*180/pi);
+
+$q = xraylib::Q_scattering_amplitude ($cryst, $energy, 1, 1, 1, $rel_angle);
+printf ("  Q Scattering amplitude: %f\n", $q);
+
+($f0, $fp, $fpp) = xraylib::Atomic_Factors (14, $energy, $q, $debye_temp_factor);
+printf ("  Atomic factors (Z = 14) f0, fp, fpp: %f, %f, i*%f\n", $f0, $fp, $fpp);
+
+$FH = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 1, 1, 1, $debye_temp_factor, $rel_angle);
+printf ("  FH(1,1,1) structure factor: (%f, %f)\n", Re($FH), Im($FH));
+
+$F0 = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 0, 0, 0, $debye_temp_factor, $rel_angle);
+printf ("  F0=FH(0,0,0) structure factor: (%f, %f)\n", Re($F0), Im($F0));
+
+# Diamond diffraction parameters
+
+$cryst = xraylib::Crystal_GetCrystal("Diamond");
+
+printf ("\nDiamond 111 at 8 KeV. Incidence at the Bragg angle:\n");
+
+$bragg = xraylib::Bragg_angle ($cryst, $energy, 1, 1, 1);
+printf ("  Bragg angle: Rad: %f Deg: %f\n", $bragg, $bragg*180/pi);
+
+$q = xraylib::Q_scattering_amplitude ($cryst, $energy, 1, 1, 1, $rel_angle);
+printf ("  Q Scattering amplitude: %f\n", $q);
+
+($f0, $fp, $fpp) =  xraylib::Atomic_Factors (6, $energy, $q, $debye_temp_factor);
+printf ("  Atomic factors (Z = 6) f0, fp, fpp: %f, %f, i*%f\n", $f0, $fp, $fpp);
+
+$FH = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 1, 1, 1, $debye_temp_factor, $rel_angle);
+printf ("  FH(1,1,1) structure factor: (%f, %f)\n", Re($FH), Im($FH));
+
+$F0 = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 0, 0, 0, $debye_temp_factor, $rel_angle);
+printf ("  F0=FH(0,0,0) structure factor: (%f, %f)\n", Re($F0), Im($F0));
+
+$FHbar = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, -1, -1, -1, $debye_temp_factor, $rel_angle);
+$dw = 1e10 * 2 * ($xraylib::R_E / $cryst->{volume}) * ($xraylib::KEV2ANGST * $xraylib::KEV2ANGST/ ($energy * $energy)) * 
+                                                  sqrt(abs($FH * $FHbar)) / pi / sin(2*$bragg);
+printf ("  Darwin width: %f micro-radians\n", 1e6*$dw);
+
+# Alpha Quartz diffraction parameters
+
+$cryst = xraylib::Crystal_GetCrystal("AlphaQuartz");
+
+printf ("\nAlpha Quartz 020 at 8 KeV. Incidence at the Bragg angle:\n");
+
+$bragg = xraylib::Bragg_angle ($cryst, $energy, 0, 2, 0);
+printf ("  Bragg angle: Rad: %f Deg: %f\n", $bragg, $bragg*180/pi);
+
+$q = xraylib::Q_scattering_amplitude ($cryst, $energy, 0, 2, 0, $rel_angle);
+printf ("  Q Scattering amplitude: %f\n", $q);
+
+($f0, $fp, $fpp) = xraylib::Atomic_Factors (8, $energy, $q, $debye_temp_factor);
+printf ("  Atomic factors (Z = 8) f0, fp, fpp: %f, %f, i*%f\n", $f0, $fp, $fpp);
+
+$FH = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 0, 2, 0, $debye_temp_factor, $rel_angle);
+printf ("  FH(0,2,0) structure factor: (%f, %f)\n", Re($FH), Im($FH));
+
+$F0 = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 0, 0, 0, $debye_temp_factor, $rel_angle);
+printf ("  F0=FH(0,0,0) structure factor: (%f, %f)\n", Re($F0), Im($F0));
+
+# Muscovite diffraction parameters 
+
+$cryst = xraylib::Crystal_GetCrystal("Muscovite");
+
+printf ("\nMuscovite 331 at 8 KeV. Incidence at the Bragg angle:\n");
+
+$bragg = xraylib::Bragg_angle ($cryst, $energy, 3, 3, 1);
+printf ("  Bragg angle: Rad: %f Deg: %f\n", $bragg, $bragg*180/pi);
+
+$q = xraylib::Q_scattering_amplitude ($cryst, $energy, 3, 3, 1, $rel_angle);
+printf ("  Q Scattering amplitude: %f\n", $q);
+
+($f0, $fp, $fpp) = xraylib::Atomic_Factors (19, $energy, $q, $debye_temp_factor);
+printf ("  Atomic factors (Z = 19) f0, fp, fpp: %f, %f, i*%f\n", $f0, $fp, $fpp);
+
+$FH = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 3, 3, 1, $debye_temp_factor, $rel_angle);
+printf ("  FH(3,3,1) structure factor: (%f, %f)\n", Re($FH), Im($FH));
+
+$F0 = xraylib::Crystal_F_H_StructureFactor ($cryst, $energy, 0, 0, 0, $debye_temp_factor, $rel_angle);
+printf ("  F0=FH(0,0,0) structure factor: (%f, %f)\n", Re($F0), Im($F0));
+
 printf("\n--------------------------- END OF XRLEXAMPLE2 -------------------------------\n");
 exit 0;
