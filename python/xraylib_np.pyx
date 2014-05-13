89cc37a93aab56be01ed791c990afe5eb546e4f7
Tom Schoonjans <tomschoonjans@uniss.it>
2014-05-13 11:17:56 +0200

    trying function factories, seems to work fine

--- python/xraylib_np.pyx
+++ python/xraylib_np.pyx
@@ -7,8 +7,6 @@ cimport cython
 K_SHELL = xrl.K_SHELL
 L1_SHELL = xrl.L1_SHELL
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
 def AtomicWeight(np.ndarray[np.int_t, ndim=1] Z not None):
 	#cdef np.ndarray[double] Zcopy = np.reshape(Z, Z.size, order='C')
 	cdef np.ndarray[double, ndim=1, mode='c'] AW = np.empty((Z.shape[0]))
@@ -16,3 +14,17 @@ def AtomicWeight(np.ndarray[np.int_t, ndim=1] Z not None):
 		AW[i] = xrl.AtomicWeight(Z[i])
 	return AW
 
+
+
+def XRL_1I(fun_wrap):
+	def fun(np.ndarray[np.int_t, ndim=1] arg1 not None):
+		cdef np.ndarray[double, ndim=1, mode='c'] rv = np.empty((arg1.shape[0]))
+		for i in range(arg1.shape[0]):
+			rv[i] = fun_wrap(arg1[i])
+		return rv
+	return fun
+
+def _ElementDensity(np.int_t arg1): 
+	return xrl.ElementDensity(arg1)
+
+ElementDensity = XRL_1I(_ElementDensity)
--- python/xraylib_np_c.pxd
+++ python/xraylib_np_c.pxd
@@ -1,4 +1,5 @@
 cdef extern from "xraylib.h":
 	double AtomicWeight(int Z)
+	double ElementDensity(int Z)
 	int K_SHELL "K_SHELL"
 	int L1_SHELL "L1_SHELL"
