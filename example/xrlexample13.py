18e766f42c5fe576025ca56610961dd326133165
Tom Schoonjans <tomschoonjans@uniss.it>
2014-05-13 18:17:30 +0200

    Example and some auxiliary functions added
    make distcheck works

---
+++ example/xrlexample13.py
@@ -0,0 +1,33 @@
+#Copyright (c) 2014 Tom Schoonjans
+#All rights reserved.
+
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#    * The names of the contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+
+#THIS SOFTWARE IS PROVIDED BY Tom Schoonjans ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Tom Schoonjans BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import numpy as np
+import xraylib_np as xrl_np
+import sys, string
+
+
+
+if __name__ == '__main__' :
+	xrl_np.XRayInit()
+	xrl_np.SetErrorMessages(0)
+	print ("Example of python-numpy program using xraylib")
+
+
+	Z = np.arange(1,94,dtype=int)
+	energies = np.arange(10,10000,dtype=np.double)/100.0
+	CS = xrl_np.CS_Total_Kissel(Z,energies)
+
+
+	print ("")
+	print ("--------------------------- END OF XRLEXAMPLE13 -------------------------------")
+	print ("")
+	sys.exit(0)
