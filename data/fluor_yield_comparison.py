4773341653d5b0064f26d8878a6242e3306d3e5f
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-04-11 08:27:47 +0530

    fluor_yield.dat: K-shell fluorescence yields revision
    Alex Simionovici noticed an unusual drop in the K-shell fluorescence
    yields, which makes no sense physically.
    This was already noted in Hubbell's 2004 erratum to his 1994 paper "A
    review, bibliography, and tabulation of K, L, and higher atomic shell
    X-ray fluorescence yields. Journal of physical and chemical reference
    data, 23(2):339–364, 1994."
    
    As recommended by him, the decision was made to use the
    Bambynek/Cohen/Burhop/Hubbell fluoresence yields for the K-shell instead

---
+++ data/fluor_yield_comparison.py
@@ -0,0 +1,39 @@
+import numpy as np
+import xraylib as xrl
+import xraylib_np as xrl_np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+Z = np.arange(3, 99)
+
+yields_bad = xrl_np.FluorYield(Z, np.array([xrl_np.K_SHELL])).squeeze()
+
+yields_good = []
+
+with open('fluor_yield_revised.dat', 'r') as file:
+    for line in file:
+        [myZ, y] = line.split()
+        yields_good.append(y)
+    yields_good = np.asarray(yields_good, dtype=np.float64)
+
+
+fig = plt.figure(figsize=(7, 10))
+gs = gridspec.GridSpec(3, 1, height_ratios=[3, 1, 1])
+ax = fig.add_subplot(gs[0])
+ax.set_xlabel('Atomic number')
+ax.set_ylabel('Fluorescence yield')
+plot_bad, = ax.plot(Z, yields_bad, color='r', marker='o', label='Fitted Hubbell')
+plot_good, = ax.plot(Z, yields_good[0:Z.shape[0]], color='g', marker='o', label='Recommended Hubbell')
+ax.legend(handles=[plot_bad, plot_good], loc=4)
+
+ax = fig.add_subplot(gs[1])
+ax.set_ylabel('Absolute diff')
+ax.plot(Z, (yields_good[0:Z.shape[0]] - yields_bad), color='b', marker='o')
+
+ax = fig.add_subplot(gs[2])
+ax.set_ylabel('Relative diff')
+ax.plot(Z, (yields_good[0:Z.shape[0]] - yields_bad) / yields_good[0:Z.shape[0]], color='b', marker='o')
+
+plt.draw()
+plt.savefig('yield-comparison.png')
+plt.close()
