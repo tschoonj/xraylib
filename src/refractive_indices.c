fca3b10ead144d98de0a892a92d709fb74148fbd
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-12-15 20:39:56 +0000

    Export some additional symbols for .NET bindings

--- src/refractive_indices.c
+++ src/refractive_indices.c
@@ -150,6 +150,7 @@ xrlComplex Refractive_Index(const char compound[], double E, double density, xrl
 	return rv;
 }
 
+XRL_EXTERN
 void Refractive_Index2(const char compound[], double E, double density, xrlComplex* result, xrl_error **error);
 
 void Refractive_Index2(const char compound[], double E, double density, xrlComplex* result, xrl_error **error) {
