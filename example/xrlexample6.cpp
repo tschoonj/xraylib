72d59bb37531daf8ef2f81995db85d487f25beea
Marius Schollmeier <m.schollmeier@gmx.net>
2020-07-17 12:40:55 -0600

    Include <memory> header

--- example/xrlexample6.cpp
+++ example/xrlexample6.cpp
@@ -13,6 +13,7 @@ THIS SOFTWARE IS PROVIDED BY Tom Schoonjans ''AS IS'' AND ANY EXPRESS OR IMPLIED
 */
 
 #include <cmath>
+#include <memory>
 #include <iostream>
 #include <sstream>
 #include <vector>
