165c63ae03df28aa69d549baa3c62b96ea9a21e8
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-17 15:37:49 +0000

    C++: add all functions that do not use classes

---
+++ cplusplus/tests/test-atomiclevelwidth.cpp
@@ -0,0 +1,54 @@
+/* Copyright (c) 2020, Tom Schoonjans
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * The names of the contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY Tom Schoonjans ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Tom Schoonjans BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifdef NDEBUG
+  #undef NDEBUG
+#endif
+#include "xraylib++.h"
+#include "xraylib-error-private.h"
+#include <cmath>
+#include <cassert>
+#include <cstring>
+
+int main(int argc, char **argv) {
+	double width;
+
+	width = xrlpp::AtomicLevelWidth(26, K_SHELL);
+	assert(fabs(width - 1.19E-3) < 1E-6);
+	
+	width = xrlpp::AtomicLevelWidth(92, N7_SHELL);
+	assert(fabs(width - 0.31E-3) < 1E-8);
+	
+	try {
+		width = xrlpp::AtomicLevelWidth(185, K_SHELL);
+	}
+	catch (std::invalid_argument &e) {
+		assert(strcmp(e.what(), Z_OUT_OF_RANGE) == 0);
+	}
+	
+	try {
+		width = xrlpp::AtomicLevelWidth(26, -5);
+	}
+	catch (std::invalid_argument &e) {
+		assert(strcmp(e.what(), UNKNOWN_SHELL) == 0);
+	}
+	
+	try {
+		width = xrlpp::AtomicLevelWidth(26, N3_SHELL);
+	}
+	catch (std::invalid_argument &e) {
+		assert(strcmp(e.what(), INVALID_SHELL) == 0);
+	}
+	
+	return 0;	
+}
--- cplusplus/xraylib++.h
+++ cplusplus/xraylib++.h
@@ -5,10 +5,108 @@
 
 #include <xraylib.h>
 #include <stdexcept>
-#include <new> 
+#include <new>
+#include <complex>
+
+#define _XRL_FUNCTION_1I(_name) \
+    double _name(int arg1) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_1D(_name) \
+    double _name(double arg1) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_2ID(_name) \
+    double _name(int arg1, double arg2) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_2DD(_name) \
+    double _name(double arg1, double arg2) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_2II(_name) \
+    double _name(int arg1, int arg2) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_3IDD(_name) \
+    double _name(int arg1, double arg2, double arg3) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, arg3, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_3IID(_name) \
+    double _name(int arg1, int arg2, double arg3) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, arg3, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_3DDD(_name) \
+    double _name(double arg1, double arg2, double arg3) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, arg3, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_4IDDD(_name) \
+    double _name(int arg1, double arg2, double arg3, double arg4) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1, arg2, arg3, arg4, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_2SD(_name) \
+    double _name(const std::string &arg1, double arg2) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1.c_str(), arg2, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_3SDD(_name) \
+    double _name(const std::string &arg1, double arg2, double arg3) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1.c_str(), arg2, arg3, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
+#define _XRL_FUNCTION_4SDDD(_name) \
+    double _name(const std::string &arg1, double arg2, double arg3, double arg4) { \
+        xrl_error *error = nullptr; \
+        double rv = ::_name(arg1.c_str(), arg2, arg3, arg4, &error); \
+        _process_error(error); \
+        return rv; \
+    }
+
 
 namespace xrlpp {
-    void process_error(xrl_error *error) {
+    void _process_error(xrl_error *error) {
         if (!error)
             return;
         switch (error->code) {
@@ -21,12 +119,118 @@ namespace xrlpp {
         }
     }
 
-    double AtomicWeight(int Z) {
+    void XrayInit(void) {
+        ::XRayInit();
+    }
+
+    std::complex<double> Refractive_Index(const std::string &compound, double E, double density) {
         xrl_error *error = nullptr;
-        double rv = ::AtomicWeight(Z, &error);
-        process_error(error);
+        xrlComplex rv = ::Refractive_Index(compound.c_str(), E, density, &error);
+        _process_error(error);
+        return std::complex<double>(rv.re, rv.im);
+    }
+
+    int SymbolToAtomicNumber(const std::string &symbol) {
+        xrl_error *error = nullptr;
+        int rv = ::SymbolToAtomicNumber(symbol.c_str(), &error);
+        _process_error(error);
         return rv;
     }
+
+    std::string AtomicNumberToSymbol(int Z) {
+        xrl_error *error = nullptr;
+        char *rv = ::AtomicNumberToSymbol(Z, &error);
+        _process_error(error);
+        std::string rv2(rv);
+        ::xrlFree(rv);
+        return rv2;
+    }
+
+    _XRL_FUNCTION_1I(AtomicWeight)
+    _XRL_FUNCTION_1I(ElementDensity)
+    _XRL_FUNCTION_1D(CS_KN)
+    _XRL_FUNCTION_1D(DCS_Thoms)
+    _XRL_FUNCTION_2ID(CS_Total)
+    _XRL_FUNCTION_2ID(CS_Photo)
+    _XRL_FUNCTION_2ID(CS_Rayl)
+    _XRL_FUNCTION_2ID(CS_Compt)
+    _XRL_FUNCTION_2ID(CS_Energy)
+    _XRL_FUNCTION_2ID(CSb_Total)
+    _XRL_FUNCTION_2ID(CSb_Photo)
+    _XRL_FUNCTION_2ID(CSb_Rayl)
+    _XRL_FUNCTION_2ID(CSb_Compt)
+    _XRL_FUNCTION_2ID(FF_Rayl)
+    _XRL_FUNCTION_2ID(SF_Compt)
+    _XRL_FUNCTION_2ID(Fi)
+    _XRL_FUNCTION_2ID(Fii)
+    _XRL_FUNCTION_2ID(CS_Photo_Total)
+    _XRL_FUNCTION_2ID(CSb_Photo_Total)
+    _XRL_FUNCTION_2ID(CS_Total_Kissel)
+    _XRL_FUNCTION_2ID(CSb_Total_Kissel)
+    _XRL_FUNCTION_2II(LineEnergy)
+    _XRL_FUNCTION_2II(FluorYield)
+    _XRL_FUNCTION_2II(CosKronTransProb)
+    _XRL_FUNCTION_2II(EdgeEnergy)
+    _XRL_FUNCTION_2II(JumpFactor)
+    _XRL_FUNCTION_2II(RadRate)
+    _XRL_FUNCTION_2DD(DCS_KN)
+    _XRL_FUNCTION_2DD(DCSP_Thoms)
+    _XRL_FUNCTION_2DD(MomentTransf)
+    _XRL_FUNCTION_2DD(ComptonEnergy)
+    _XRL_FUNCTION_3IDD(DCS_Rayl)
+    _XRL_FUNCTION_3IDD(DCS_Compt)
+    _XRL_FUNCTION_3IDD(DCSb_Rayl)
+    _XRL_FUNCTION_3IDD(DCSb_Compt)
+    _XRL_FUNCTION_3DDD(DCSP_KN)
+    _XRL_FUNCTION_3IID(CS_FluorLine)
+    _XRL_FUNCTION_3IID(CSb_FluorLine)
+    _XRL_FUNCTION_3IID(CS_Photo_Partial)
+    _XRL_FUNCTION_3IID(CSb_Photo_Partial)
+    _XRL_FUNCTION_4IDDD(DCSP_Rayl)
+    _XRL_FUNCTION_4IDDD(DCSP_Compt)
+    _XRL_FUNCTION_4IDDD(DCSPb_Rayl)
+    _XRL_FUNCTION_4IDDD(DCSPb_Compt)
+    _XRL_FUNCTION_2ID(ComptonProfile)
+    _XRL_FUNCTION_3IID(ComptonProfile_Partial)
+    _XRL_FUNCTION_2II(ElectronConfig)
+    _XRL_FUNCTION_2II(AtomicLevelWidth)
+    _XRL_FUNCTION_2II(AugerRate)
+    _XRL_FUNCTION_2II(AugerYield)
+    _XRL_FUNCTION_3IID(CS_FluorLine_Kissel)
+    _XRL_FUNCTION_3IID(CSb_FluorLine_Kissel)
+    _XRL_FUNCTION_3IID(CS_FluorLine_Kissel_Cascade)
+    _XRL_FUNCTION_3IID(CSb_FluorLine_Kissel_Cascade)
+    _XRL_FUNCTION_3IID(CS_FluorLine_Kissel_no_Cascade)
+    _XRL_FUNCTION_3IID(CSb_FluorLine_Kissel_no_Cascade)
+    _XRL_FUNCTION_3IID(CS_FluorLine_Kissel_Nonradiative_Cascade)
+    _XRL_FUNCTION_3IID(CSb_FluorLine_Kissel_Nonradiative_Cascade)
+    _XRL_FUNCTION_3IID(CS_FluorLine_Kissel_Radiative_Cascade)
+    _XRL_FUNCTION_3IID(CSb_FluorLine_Kissel_Radiative_Cascade)
+
+    _XRL_FUNCTION_2SD(CS_Total_CP)
+    _XRL_FUNCTION_2SD(CS_Photo_CP)
+    _XRL_FUNCTION_2SD(CS_Rayl_CP)
+    _XRL_FUNCTION_2SD(CS_Compt_CP)
+    _XRL_FUNCTION_2SD(CS_Energy_CP)
+    _XRL_FUNCTION_2SD(CSb_Total_CP)
+    _XRL_FUNCTION_2SD(CSb_Photo_CP)
+    _XRL_FUNCTION_2SD(CSb_Rayl_CP)
+    _XRL_FUNCTION_2SD(CSb_Compt_CP)
+    _XRL_FUNCTION_3SDD(DCS_Rayl_CP)
+    _XRL_FUNCTION_3SDD(DCS_Compt_CP)
+    _XRL_FUNCTION_3SDD(DCSb_Rayl_CP)
+    _XRL_FUNCTION_3SDD(DCSb_Compt_CP)
+    _XRL_FUNCTION_4SDDD(DCSP_Rayl_CP)
+    _XRL_FUNCTION_4SDDD(DCSP_Compt_CP)
+    _XRL_FUNCTION_4SDDD(DCSPb_Rayl_CP)
+    _XRL_FUNCTION_4SDDD(DCSPb_Compt_CP)
+    _XRL_FUNCTION_2SD(CS_Photo_Total_CP)
+    _XRL_FUNCTION_2SD(CSb_Photo_Total_CP)
+    _XRL_FUNCTION_2SD(CS_Total_Kissel_CP)
+    _XRL_FUNCTION_2SD(CSb_Total_Kissel_CP)
+    _XRL_FUNCTION_3SDD(Refractive_Index_Re)
+    _XRL_FUNCTION_3SDD(Refractive_Index_Im)
+
 }
 
 #endif
